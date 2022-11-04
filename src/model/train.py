8e328785fef6c2d9b6e083a0e51e9be58182044e
Nora Abi Akar <noraabiakar@microsoft.com>
2022-11-04 13:58:45 +0100

    fix liniting errors

--- src/model/train.py
+++ src/model/train.py
@@ -34,10 +34,10 @@ def get_csvs_df(path):
     return pd.concat((pd.read_csv(f) for f in csv_files), sort=False)
 
 
-# TO DO: add function to split data
 def split_data(df):
-    X, y = df[['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness','SerumInsulin','BMI','DiabetesPedigree','Age']].values, df['Diabetic'].values
+    X, y = df[['Pregnancies', 'PlasmaGlucose', 'DiastolicBloodPressure', 'TricepsThickness', 'SerumInsulin', 'BMI', 'DiabetesPedigree', 'Age']].values, df['Diabetic'].values
     return train_test_split(X, y, test_size=0.30, random_state=0)
+
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
     # train model
@@ -59,6 +59,7 @@ def parse_args():
 
     # return args
     return args
+
 
 # run script
 if __name__ == "__main__":
