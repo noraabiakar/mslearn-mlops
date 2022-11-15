e35bdeb42218698e82f3a1ab0eb44d51fe427960
Nora Abi Akar <noraabiakar@microsoft.com>
2022-11-15 15:27:51 +0100

    try unit testing

--- src/model/train.py
+++ src/model/train.py
@@ -42,7 +42,7 @@ def split_data(df):
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
     # train model
-    LogisticRegression(C=1/reg_rate, solver="liblinear").fit(X_train, y_train)
+    LogisticRegression(C=1 / reg_rate, solver="liblinear").fit(X_train, y_train)
 
 
 def parse_args():
